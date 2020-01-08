12461d8aa9c94531a4b562f839f83a7122570af6
Rello <github@scherello.de>
2020-01-08 20:39:02 +0100

    NC 18 Workflow integration #10

--- js/flow.js
+++ js/flow.js
@@ -1,4 +1,44 @@
-window.OCA.WorkflowEngine.registerOperator({
-    id: 'OCA\\Analytics\\Flow\\Operation',
-    color: 'var(--color-success)',
-});
+(function () {
+
+    var Component = {
+        name: 'WorkflowScript',
+        render: function (createElement) {
+            var self = this;
+            return createElement('div', {
+                style: {
+                    width: '100%'
+                },
+            }, [
+                createElement('select', {
+                        style: {
+                            width: '100%'
+                        },
+                    }, [
+                        createElement('option', {
+                            domProps: {
+                                value: '1',
+                                innerText: 'Test Report'
+                            },
+                        })
+                    ]
+                ),
+            ])
+        },
+        props: {
+            value: ''
+        },
+        data: function () {
+            return {
+                description: t('workflow_script', 'Available placeholder variables are listed in the documentation') + '↗',
+                link: 'https://github.com/nextcloud/workflow_script#placeholders'
+            }
+        }
+    };
+
+    window.OCA.WorkflowEngine.registerOperator({
+        id: 'OCA\\Analytics\\Flow\\Operation',
+        color: 'var(--color-success)',
+        operation: '',
+        options: Component,
+    });
+})();
