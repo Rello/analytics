f53f36e6939e9d7ef1f2ff4d611ec18f25fe0782
Rello <github@scherello.de>
2021-03-14 22:27:49 +0100

    3.4.1

--- lib/Migration/Version030400Date20210313200000.php
+++ lib/Migration/Version030400Date20210313200000.php
@@ -54,7 +54,7 @@ class Version030400Date20210313200000 extends SimpleMigrationStep
         $query = $this->connection->getQueryBuilder();
         $query->insert('analytics_whats_new')
             ->values([
-                'version' => $query->createNamedParameter('3.4.0'),
+                'version' => $query->createNamedParameter('3.4.1'),
                 'data' => $query->createNamedParameter('{"changelogURL":"https:\/\/github.com\/rello\/analytics\/blob\/master\/CHANGELOG.md","whatsNew":{
 "en":{"regular":["Datasource for spreadsheets (xls, xlsx, ods)","Times are shown in local timezone","UI improvements"],"admin":["New Features apply to users"]},
 "de":{"regular":["Datenquelle für Tabellen (xls, xlsx, ods)","Zeiten werden in der lokalen Zeitzohne gezeigt","UI Verbesserungen"],"admin":["Nur User Features"]}
