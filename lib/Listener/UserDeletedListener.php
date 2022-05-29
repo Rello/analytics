86c1b00d193bb4aac1d00679b9f1f3ce1acf0297
Rello <github@scherello.de>
2022-05-29 22:07:46 +0200

    remove data when user is deleted #280

---
+++ lib/Listener/UserDeletedListener.php
@@ -0,0 +1,39 @@
+<?php
+/**
+ * Analytics
+ *
+ * This file is licensed under the Affero General Public License version 3 or
+ * later. See the LICENSE.md file.
+ *
+ * @author Marcel Scherello <analytics@scherello.de>
+ * @copyright 2021 Marcel Scherello
+ */
+
+declare(strict_types=1);
+
+namespace OCA\Analytics\Listener;
+
+use OCP\EventDispatcher\Event;
+use OCP\EventDispatcher\IEventListener;
+use OCP\User\Events\UserDeletedEvent;
+use OCA\Analytics\Service\UserService;
+
+class UserDeletedListener implements IEventListener
+{
+
+    /** @var UserService */
+    private $service;
+
+    public function __construct(UserService $service) {
+        $this->service = $service;
+    }
+    public function handle(Event $event): void
+    {
+        if(!$event instanceof UserDeletedEvent) {
+            return;
+        }
+
+        $uid = $event->getUser()->getUID();
+        $this->service->deleteUserData($uid);
+    }
+}
