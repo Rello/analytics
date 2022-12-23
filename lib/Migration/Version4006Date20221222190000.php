c9d104a6a33cf25492e69de0f24c2047a188130d
Rello <github@scherello.de>
2022-12-23 22:04:02 +0100

    4.6.0

---
+++ lib/Migration/Version4006Date20221222190000.php
@@ -0,0 +1,65 @@
+<?php
+
+declare(strict_types=1);
+
+namespace OCA\Analytics\Migration;
+
+use Closure;
+use OCP\DB\ISchemaWrapper;
+use OCP\IDBConnection;
+use OCP\Migration\IOutput;
+use OCP\Migration\SimpleMigrationStep;
+
+/**
+ * Auto-generated migration step: Please modify to your needs!
+ * sudo -u www-data php /var/www/nextcloud/occ migrations:execute analytics 4006Date20221222190000
+ */
+class Version4006Date20221222190000 extends SimpleMigrationStep
+{
+
+    /** @var IDBConnection */
+    private $connection;
+
+    public function __construct(IDBConnection $connection)
+    {
+        $this->connection = $connection;
+    }
+
+    /**
+     * @param IOutput $output
+     * @param Closure $schemaClosure The `\Closure` returns a `ISchemaWrapper`
+     * @param array $options
+     */
+    public function preSchemaChange(IOutput $output, Closure $schemaClosure, array $options)
+    {
+    }
+
+    /**
+     * @param IOutput $output
+     * @param Closure $schemaClosure The `\Closure` returns a `ISchemaWrapper`
+     * @param array $options
+     * @return null|ISchemaWrapper
+     */
+    public function changeSchema(IOutput $output, Closure $schemaClosure, array $options)
+    {
+    }
+
+    /**
+     * @param IOutput $output
+     * @param Closure $schemaClosure The `\Closure` returns a `ISchemaWrapper`
+     * @param array $options
+     */
+    public function postSchemaChange(IOutput $output, Closure $schemaClosure, array $options)
+    {
+        $query = $this->connection->getQueryBuilder();
+        $query->insert('analytics_whats_new')
+            ->values([
+                'version' => $query->createNamedParameter('4.6.0'),
+                'data' => $query->createNamedParameter('{"changelogURL":"https:\/\/github.com\/rello\/analytics\/blob\/master\/CHANGELOG.md","whatsNew":{
+"en":{"regular":["Notifications for failed data loads","Schedule deletion jobs per dataset to clean up old data","Various fixes"],"admin":["New Features apply to users"]},
+"de":{"regular":["Benachrichtigung bei fehlerhaften Datenladungen","Plane Löschjobs je Datensatz um alte Daten zu bereinigen","Verschiedene Korrekturen"],"admin":["Nur User Features"]}
+}}'),
+            ])
+            ->execute();
+    }
+}
