0348f2a50469ee20cf7ea15dc8fa37f5bde21fc6
Rello <github@scherello.de>
2023-05-26 08:13:05 +0200

    Sharing reports not possible any more #354

--- lib/Service/ShareService.php
+++ lib/Service/ShareService.php
@@ -148,7 +148,7 @@ class ShareService
         $reports = array();
 
         foreach ($sharedReports as $sharedReport) {
-            $this->logger->debug('Shareservice: evaluating share: ' . $sharedReport["id"] . ' type ' . $sharedReport['shareType'] . ' for '. $sharedReport['shareUid_owner']);
+            $this->logger->debug('Shareservice: evaluating share: ' . $sharedReport["shareId"] . ' of report ' . $sharedReport["id"] . ' type ' . $sharedReport['shareType'] . ' for '. $sharedReport['shareUid_owner']);
             // shared with a group?
             if ($sharedReport['shareType'] === self::SHARE_TYPE_GROUP) {
                 // is the current user part of this group?
@@ -165,7 +165,7 @@ class ShareService
             // shared with a user directly?
             } elseif ($sharedReport['shareType'] === self::SHARE_TYPE_USER) {
                 // current user matching?
-                $this->logger->debug('Shareservice: Type user; check against current user ' . $this->userSession->getUser()->getUID());
+                $this->logger->debug('Shareservice: Type user; check against current user: ' . $this->userSession->getUser()->getUID());
                 if ($this->userSession->getUser()->getUID() === $sharedReport['shareUid_owner']) {
                     // was the report not yet added to the result?
                     $this->logger->debug('Shareservice: Share belongs to current user');
