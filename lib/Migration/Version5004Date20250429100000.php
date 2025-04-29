b98eabaa9f97463b5f28ed904c0239944d5b126e
Rello <github@scherello.de>
2025-04-29 15:12:17 +0200

    5.4.0

---
+++ lib/Migration/Version5004Date20250429100000.php
@@ -0,0 +1,44 @@
+<?php
+/**
+ * Analytics
+ *
+ * SPDX-FileCopyrightText: 2019-2022 Marcel Scherello
+ * SPDX-License-Identifier: AGPL-3.0-or-later
+ */
+
+declare(strict_types=1);
+
+namespace OCA\Analytics\Migration;
+
+use Closure;
+use OCP\DB\ISchemaWrapper;
+use OCP\IDBConnection;
+use OCP\Migration\IOutput;
+use OCP\Migration\SimpleMigrationStep;
+
+/**
+ * Auto-generated migration step: Please modify to your needs!
+ * sudo -u www-data php occ migrations:execute analytics 5004Date20250429100000
+ */
+class Version5004Date20250429100000 extends SimpleMigrationStep {
+	public function __construct(
+		private IDBConnection $connection,
+	) {
+	}
+
+	/**
+	 * @param IOutput $output
+	 * @param Closure $schemaClosure The `\Closure` returns a `ISchemaWrapper`
+	 * @param array $options
+	 */
+	public function postSchemaChange(IOutput $output, Closure $schemaClosure, array $options) {
+		$query = $this->connection->getQueryBuilder();
+		$query->insert('analytics_whats_new')->values([
+				'version' => $query->createNamedParameter('5.4.0'),
+				'data' => $query->createNamedParameter('{"changelogURL":"https:\/\/github.com\/rello\/analytics\/blob\/master\/CHANGELOG.md","whatsNew":{
+"en":{"regular":["Funnel chart","Sorting by any column","Improved aggregation of data"],"admin":["New Features apply to users"]},
+"de":{"regular":["Funnel Chart","Sortierung für alle Spalten","Verbesserte Aggregation von Daten"],"admin":["Nur User Features"]}
+}}'),
+			])->executeStatement();
+	}
+}
