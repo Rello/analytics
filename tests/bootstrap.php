2e20cdb4a3cb4555d56dbbce007c7caaefe98d34
Rello <github@scherello.de>
2025-06-06 08:17:50 +0200

    fine tuning

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -1,11 +1,16 @@
 <?php
 require __DIR__ . '/../vendor/autoload.php';
 
+$prefixes = [
+	'Psr\\Log\\' => __DIR__.'/Stubs/Psr/Log/',
+];
+
 spl_autoload_register(function ($class) {
     $prefixes = [
         'OCA\\Analytics\\' => __DIR__ . '/../lib/',
         'OCA\\Analytics\\Tests\\' => __DIR__ . '/',
         'OCP\\' => __DIR__ . '/Stubs/OCP/',
+		'OCP\\' => __DIR__ . '/Stubs/Psr/',
     ];
     foreach ($prefixes as $prefix => $baseDir) {
         if (strncmp($class, $prefix, strlen($prefix)) === 0) {
