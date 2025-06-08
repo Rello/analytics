0744fa36cdee1335f9237fa37d220a642b3b45b8
Rello <github@scherello.de>
2025-06-08 11:14:40 +0200

    5.6.0

---
+++ lib/Migration/Version5006Date20250528100000.php
@@ -0,0 +1,44 @@
+<?php
+/**
+ * Analytics
+ *
+ * SPDX-FileCopyrightText: 2019-2022 Marcel Scherello
+ * SPDX-License-Identifier: AGPL-3.0-or-later
+ */
+
+declare(strict_types=1);
+
+namespace OCA\Analytics\Migration;
+
+use Closure;
+use OCP\DB\ISchemaWrapper;
+use OCP\IDBConnection;
+use OCP\Migration\IOutput;
+use OCP\Migration\SimpleMigrationStep;
+
+/**
+ * Auto-generated migration step: Please modify to your needs!
+ * sudo -u www-data php occ migrations:execute analytics 5006Date20250528100000
+ */
+class Version5006Date20250528100000 extends SimpleMigrationStep {
+	public function __construct(
+		private IDBConnection $connection,
+	) {
+	}
+
+	/**
+	 * @param IOutput $output
+	 * @param Closure $schemaClosure The `\Closure` returns a `ISchemaWrapper`
+	 * @param array $options
+	 */
+	public function postSchemaChange(IOutput $output, Closure $schemaClosure, array $options) {
+		$query = $this->connection->getQueryBuilder();
+		$query->insert('analytics_whats_new')->values([
+				'version' => $query->createNamedParameter('5.6.0'),
+				'data' => $query->createNamedParameter('{"changelogURL":"https:\/\/github.com\/rello\/analytics\/blob\/master\/CHANGELOG.md","whatsNew":{
+"en":{"regular":["Thresholds were redesigned", "Existing thresholds are impacted", "Check the changelog", "Sorting & editing is now available"],"admin":["New Features apply to users"]},
+"de":{"regular":["Schwellenwerte wurden überarbeitet", "Bestehende Settings betroffen", "Bitte Changelog prüfen", "Sortieren & Bearbeiten ist jetzt möglich"],"admin":["Nur User Features"]}
+}}'),
+			])->executeStatement();
+	}
+}
